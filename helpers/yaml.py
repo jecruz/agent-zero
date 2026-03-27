2bf36608a83b0caa67d53da182e6e23e69657ea9
Jeffrey Cruz <jeffrey.cruz@me.com>
2026-03-27 07:07:38 -0400

    fix: add safe_load alias to helpers/yaml wrapper

---
+++ helpers/yaml.py
@@ -0,0 +1,28 @@
+import json
+import yaml
+
+
+def safe_load(text):
+    return yaml.safe_load(text)
+
+
+# Alias for compatibility
+loads = safe_load
+
+
+def dumps(obj, **kwargs) -> str:
+    dump_kwargs = {
+        "allow_unicode": True,
+        "sort_keys": False,
+        **kwargs,
+    }
+    return yaml.safe_dump(obj, **dump_kwargs)
+
+
+def from_json(text: str, **yaml_dump_kwargs) -> str:
+    return dumps(json.loads(text), **yaml_dump_kwargs)
+
+
+def to_json(text: str, **json_dump_kwargs) -> str:
+    obj = loads(text)
+    return json.dumps(obj, ensure_ascii=False, **json_dump_kwargs)
